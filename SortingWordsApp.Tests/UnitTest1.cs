5d72755d11e6a6d60856ba45a37af1e2a1cb2cd8
jsabithabanu <jsabithabanu@gmail.com>
2022-09-12 11:25:02 +0100

    Initial commit for the Sorting Words App

---
+++ SortingWordsApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SortingWordsApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
